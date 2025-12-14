8180b27a00f0583bd31892ac34a78ed0b5cc1d48
takaken94 <takaken94@gmail.com>
2025-12-14 13:58:21 +0900

    feat: Add CSV and Parquet file export

--- getting_started.py
+++ getting_started.py
@@ -13,7 +13,7 @@ def load_dataframe(file_path: str, encoding: str, has_header: bool = False, sepa
         print(f"エラー: ファイルが見つかりません: {file_path}")
         exit(1)
     except Exception as e:
-        print(f"エラー: ファイル読み込み中に問題が発生しました: {e}")
+        print(f"エラー: ファイル読み込み中にエラーが発生しました: {e}")
         exit(1)
 
 def main():
@@ -27,6 +27,10 @@ def main():
     col_name_file_path = "data/mst_column_name.txt"
     col_name_file_encoding = "shift-jis"
     target_col_name_in_file = "column_name_en"
+
+    # 出力ファイル
+    out_csv_file_path = "data/22_shizuoka_all.csv"
+    out_parq_file_path = "data/22_shizuoka_all.parquet"
 
     # --- 処理 ---
     # データの読み込み
@@ -48,11 +52,20 @@ def main():
     # pl.Config.set_tbl_cols(-1)  # 表示する列数を無制限に設定
 
     # 統計量と先頭データの表示
-    print("--- 基本統計量 ---")
-    print(df.describe())
-    print("\n--- データプレビュー (先頭5行) ---")
-    print(df.head())
+    # print(df.describe())
+    # print(df.head())
 
+    # --- 処理 ---
+    # データの保存
+    try:
+        df.write_csv(out_csv_file_path)
+    except Exception as e:
+        print(f"エラー: CSVファイルの保存中にエラーが発生しました: {e}")
+
+    try:
+        df.write_parquet(out_parq_file_path)
+    except Exception as e:
+        print(f"エラー: Parquetファイルの保存中にエラーが発生しました: {e}")
 
 if __name__ == "__main__":
     main()
